175d30afc300d02b47a776bf215fa3f82f392410
zida-bi <zida_bi@hotmail.com>
2015-02-24 12:38:30 -0500

    HomePage-Complete

--- Assignment02/Scripts/_references.js
+++ Assignment02/Scripts/_references.js
@@ -2,3 +2,6 @@
 /// <reference path="app.js" />
 /// <reference path="jquery-1.8.0.js" />
 /// <reference path="jquery.mobile-1.4.5.js" />
+/// <reference path="../sliderengine/jquery.js" />
+/// <reference path="../sliderengine/initslider-1.js" />
+/// <reference path="../sliderengine/amazingslider.js" />
